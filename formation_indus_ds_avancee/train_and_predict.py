519d7a732990126cba5af2cea0740c1d31299f6a
Giloh7 <valentin.gillot7@gmail.com>
2024-06-18 12:23:23 +0000

    empty

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -4,6 +4,7 @@ import time
 import joblib
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+from datetime import datetime
 
 
 def train_model_with_io(features_path: str, model_registry_folder: str) -> None:
@@ -18,7 +19,10 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     y = features[target]
     model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
     model.fit(X, y)
-    joblib.dump(model, os.path.join(model_registry_folder, 'model.joblib'))
+    # Get the current timestamp and format it
+    timestamp = datetime.now().strftime('%Y%m%d-%H%M%S')
+
+    joblib.dump(model, os.path.join(model_registry_folder, f'model_{timestamp}.joblib'))
 
 
 def predict_with_io(features_path: str, model_path: str, predictions_folder: str) -> None:
